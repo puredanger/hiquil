41863c03e2b4372860e0386f19bfbae033aeab80
puredanger <alex.miller@cognitect.com>
2016-12-21 11:34:38 -0600

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# hiquil
+
+A runnable Quil example.
+
+## License
+
+Copyright © 2016 Alex Miller
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject puredanger/hiquil "0.1.0"
+  :description "Runnable quil example"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.5.0"]]
+  :main hiquil.core
+  :aot :all)
